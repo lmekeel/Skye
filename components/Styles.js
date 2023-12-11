000deb08d66b3f2b168134c015a6efe1197da998
lmekeel <95368109+lmekeel@users.noreply.github.com>
2023-12-11 14:19:50 -0600

    installed and set up RN Paper for UI

---
+++ components/Styles.js
@@ -0,0 +1,13 @@
+//Style sheet to maintain consistent styles across different components
+
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default styles;

732bbd4c492eff0d967d6bfc7f99b38d76af8047
lmekeel <95368109+lmekeel@users.noreply.github.com>
2023-12-12 18:09:04 -0600

    Added React Navigation and set up nav between login and registration pages

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import * as React from "react";
-import { View, AppRegistry, Text} from "react-native";
+import {AppRegistry} from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { name as SkyeTest } from "./app.json";
 import theme from "./components/PaperProvider";
-import styles from "./components/Styles";
 import Registration from "./screens/Registration";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     // The paper provider wraps all UI components with React Native Paper
-    <PaperProvider theme={theme}> 
-      
-        <Registration />
-      
+    //The Navigation Container handles app navigation
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Registration">
+          <Stack.Screen
+          //options removes headers from screens
+            options={{ header: () => null }}
+            name="Registration"
+            component={Registration}
+          />
+          <Stack.Screen
+            options={{ header: () => null }} 
+            name="Login"
+            component={Login}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
